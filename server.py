c0e865a4ae0b25035662dcd443b36f5324830a57
Cody Johnson <codyjohnsontx@gmail.com>
2023-08-19 15:44:20 -0500

    continue build outwith modification and addition of files

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
+import os
 app = Flask(__name__)
 
 print(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/<username>/<int:post_id>")
+def hello_world(username=None, post_id=None):
+    return render_template('index.html', name=username, post_id=post_id)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+"""
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+                              """ 
+
+
+@app.route('/blogs/2020/dogs')
+def blog2():
+    return 'WOOF WOOF MY DUDE'
