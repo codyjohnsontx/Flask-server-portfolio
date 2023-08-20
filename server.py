971844d12c4043917ef3b23e9422f7549a6fafef
Cody Johnson <codyjohnsontx@gmail.com>
2023-08-20 02:38:58 -0500

    add new background and link html pages correctly

--- server.py
+++ server.py
@@ -1,28 +1,25 @@
 from flask import Flask, render_template, url_for
-import os
+
 app = Flask(__name__)
 
 print(__name__)
 
+@app.route("/")
+def my_home():
+    return render_template('index.html')
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    return render_template('index.html', name=username, post_id=post_id)
+@app.route("/index.html")
+def index():
+    return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about.html')
 def about():
     return render_template('about.html')
 
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
 
-"""
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-                              """ 
-
-
-@app.route('/blogs/2020/dogs')
-def blog2():
-    return 'WOOF WOOF MY DUDE'
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
