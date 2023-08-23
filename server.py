dd79ecf97f1938c40259d34f186669755db57588
Cody Johnson <codyjohnsontx@gmail.com>
2023-08-23 00:52:42 -0500

    Add csv file, adjust .html files

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -8,18 +9,35 @@ print(__name__)
 def my_home():
     return render_template('index.html')
 
-@app.route("/index.html")
-def index():
-    return render_template('index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data.get('email', '')
+        subject = data.get('subject', '')
+        message = data.get('message', '')
+        database.write(f'\n{email},{subject},{message}')
 
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
+def write_to_csv(data):
+    with open('database.csv', mode='a',newline='') as database2:
+        email = data.get('email')
+        subject = data.get('subject')
+        message = data.get('message')
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
 
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return "Didn't save to database" 
+    else:
+        return 'something went wrong, try again'
+    #return render_template('login.html', error=error)
+
